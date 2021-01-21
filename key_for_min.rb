258d47a608ee665ae7e5db0dd30b84be445fe457
SuperPox <calebkohl@gmail.com>
2021-01-20 18:18:17 -0700

    commit tree 8d00d50524abe3c4e5328b1cfb21a204e0bdea55

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,27 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
+# {:blake => 500, :ashley => 2, :adam => 1}
+# get the values, find the smallest value, return that values key
+# require 'pry'
+# def key_for_min_value(name_hash)
+#     name_hash.collect do |key, value|
+#         value
+#     end
 
+# end
+
+    
 def key_for_min_value(name_hash)
-
+    lastValue = 1000
+    lastKey = nil
+    name_hash.collect do |key, value|
+        if value < lastValue
+            lastValue = value
+            lastKey = key
+        else
+            lastValue = lastValue
+            lastKey = lastKey
+        end
+    end
+    return lastKey
 end
